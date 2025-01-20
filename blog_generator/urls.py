d0e49997b844ada55a70c400509378ce38a216d9
Sahil Gunjal <gunjal.zswyv21@sinhgad.edu>
2025-01-20 19:29:44 +0530

    Signup Page Implementated

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('login/',views.user_login,name='login'),
+    path('signup/',views.user_signup,name='signup'),
+    path('logout/',views.user_logout,name='logout'),
+
 ]
