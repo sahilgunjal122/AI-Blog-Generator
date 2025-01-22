b4e12b3a0193cc3890e858c2fe7003f694ef3d28
Sahil Gunjal <gunjal.zswyv21@sinhgad.edu>
2025-01-22 11:14:55 +0530

    Logout implemented

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request,'index.html')
 
@@ -45,4 +47,5 @@ def user_signup(request):
     return render(request,'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
