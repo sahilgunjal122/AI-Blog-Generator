ad75bdb0c4a2878d28cb1c68f88dc66146a2a0db
Sahil Gunjal <gunjal.zswyv21@sinhgad.edu>
2025-01-25 10:58:54 +0530

    All AI Logic is Applied

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,4 +9,5 @@ urlpatterns = [
     path('login/',views.user_login,name='login'),
     path('signup/',views.user_signup,name='signup'),
     path('logout/',views.user_logout,name='logout'),
+    path('generate-blog/', views.generate_blog, name='generate-blog'),
 ]
